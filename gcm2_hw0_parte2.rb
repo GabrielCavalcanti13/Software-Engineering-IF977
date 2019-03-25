01fe22e51247dac0d421ab8fc05d58361104ae9c
Gabriel Cavalcanti <gabrielnick98@gmail.com>
2019-03-25 15:17:27 -0300

    lista 1 parte 2 letra b

--- gcm2_hw0_parte2.rb
+++ gcm2_hw0_parte2.rb
@@ -6,3 +6,17 @@ def hello(name)
     return "Hello, #{name}"
 end
 
+def starts_with_consonant?(s)
+     vowels= ["a","e","i","o","u"]
+     for x in vowels
+          if x == s[0] || x.capitalize == s[0]
+               return false
+          end
+     end
+     if s[0] > "b" && s[0] < "z" || s[0] > "B" && s[0] < "Z"
+          return true
+     end
+     return false 
+ end
+     
+        
