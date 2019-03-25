11f8314d58c7ad5efd028414ee64771501439ab4
Gabriel Cavalcanti <gabrielnick98@gmail.com>
2019-03-25 14:10:49 -0300

    lista 1 parte 1

--- gcm2_hw0_parte1.rb
+++ gcm2_hw0_parte1.rb
@@ -1,3 +1,37 @@
 # Author: Gabriel Cavalcanti 
 # email: gcm2@cin.ufpe.br
 # date: 23-mar-2019
+
+def sum(array)
+    result = 0
+    for x in array
+        result += x
+    end
+    return result
+end
+
+def max_2_sum(array)
+    result = 0
+    for i in 0..1
+        max = array.max
+        for x in 0...array.length 
+            if array[x] == max
+                result += array[x]
+                array.delete_at(x)
+                break
+            end
+        end
+    end
+    return result
+end
+
+def sum_to_n?(array,n)
+    for i in 0...array.length
+        for x in i+1...array.length
+            if array[i]+array[x] == n 
+                return true
+            end
+        end
+    end
+    return false
+end
---
+++ gcm2_hw0_parte2.rb
@@ -0,0 +1,8 @@
+# Author: Gabriel Cavalcanti 
+# email: gcm2@cin.ufpe.br
+# date: 23-mar-2019
+
+def hello(name)
+    return "Hello, #{name}"
+end
+
